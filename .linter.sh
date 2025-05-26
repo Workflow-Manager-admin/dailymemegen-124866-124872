e2d11496a08a8c847d831351921f09099af0e781
Gerardo Rios <gerardo@kavia.ai>
2025-05-26 11:40:45 -0500

    Auto-commit: Changes as of 2025-05-26T16:40:45.573392+00:00

---
+++ .linter.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /tmp/kavia/workspace/code-generation/dailymemegen-124866-124872/daily_meme_gen
+flutter analyze
+
---
+++ daily_meme_gen/android/app/src/main/kotlin/com/example/daily_meme_gen/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.daily_meme_gen
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
